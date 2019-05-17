9b47d8208ecc9f294ec7fbdc2517748ab87978fc
L3D <l3d@see-base.de>
2019-05-17 09:12:10 +0200

    meh

--- delete_history.sh
+++ delete_history.sh
@@ -7,12 +7,8 @@ slack-cleaner --token $token --message --channel floppy-orgel --user "l3d" --bef
 slack-cleaner --token $token --message --channel general --user "l3d" --before $timee --rate=5 --perform
 slack-cleaner --token $token --message --channel hardware --user "l3d" --before $timee --rate=5 --perform
 slack-cleaner --token $token --message --channel infrastruktur --user "l3d" --before $timee --rate=5 --perform
-slack-cleaner --token $token --message --channel innenarchitektur --user "l3d" --before $timee --rate=5 --perform
-slack-cleaner --token $token --message --channel jobs --user "l3d" --before $timee --rate=5 --perform
-slack-cleaner --token $token --message --channel makerfaire --user "l3d" --before $timee --rate=5 --perform
 slack-cleaner --token $token --message --channel security --user "l3d" --before $timee --rate=5 --perform
 slack-cleaner --token $token --message --channel siebdruck --user "l3d" --before $timee --rate=5 --perform
-slack-cleaner --token $token --message --channel streaming --user "l3d" --before $timee --rate=5 --perform
-slack-cleaner --token $token --message --channel cabal --user "l3d" --before $tim3 --rate=5 --perform
+slack-cleaner --token $token --message --channel tbwebsite --user "l3d" --before $tim3 --rate=5 --perform
 slack-cleaner --token $token --message --channel marktplatz --user "l3d" --before $tim3 --rate=5 --perform
 
